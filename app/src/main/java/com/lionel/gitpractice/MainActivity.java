4584bf838e8cae13c3e8bcf8f4c561070e4bc037
l10nel <l10ne1>
2019-05-17 17:00:50 +0800

    develop 1

--- app/src/main/java/com/lionel/gitpractice/MainActivity.java
+++ app/src/main/java/com/lionel/gitpractice/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("<>", "master 1");
+        Log.d("<>", "develop 1");
     }
 }
