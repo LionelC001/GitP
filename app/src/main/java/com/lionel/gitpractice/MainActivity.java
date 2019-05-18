a4c80d46f810a992a730856fd476f479ed8513e0
LionelC001 <lionelc15001@gmail.com>
2019-05-18 18:08:12 +0800

    develop 9

--- app/src/main/java/com/lionel/gitpractice/MainActivity.java
+++ app/src/main/java/com/lionel/gitpractice/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("<>", "master 2");
-        Log.d("<>", "develop 8");
+        Log.d("<>", "develop 9 on website");
 
 
         Log.d("<>", "feature1 done -->  feature1 fixed");
@@ -27,5 +27,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d("<>", "feature 4.1 done");
 
         Log.d("<>", "feature 5 done");
+        
+     
     }
 }
